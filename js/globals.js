3a92dc4f0cc6ee09060378ff30b3bc06f45d9f8e
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2020-04-30 18:51:57 -0700

    Initial commit

---
+++ js/globals.js
@@ -0,0 +1,5 @@
+// Just a container for variables shared across scripts.
+
+let stage;
+let table;
+let reference = 'models/template/6m17_BE_wSugar.pdb';
---
+++ js/make_table.js
@@ -0,0 +1,45 @@
+table = new Tabulator("#models-table", {
+	
+	// Data
+	ajaxURL: "models/models.json",
+	ajaxContentType: "json",
+	ajaxResponse: function(url, params, response){
+		return response;
+	},
+	// Formatting
+    columns: [
+        {title:"Model Name", field:"name", responsive: 0, widthGrow: 2},
+        // {title:"Species", field:"species", responsive: 0, widthGrow: 1},
+        {title:"Score", field:"haddock_score", responsive: 0},
+        {
+        	title:"Detailed Energetics", // grouped
+        	columns: [
+		        {title:"van der Waals", field:"e_vdw"},
+		        {title:"Electrostatics", field:"e_elec"},
+		        {title:"Desolvation", field:"e_desolv"},
+		        {title:"BSA", field:"buried_surf_area"},
+        	]
+        }
+    ],
+    // Layout
+    layout:"fitColumns",
+    resizableColumns: false,
+	groupBy:"species",
+    selectable: true,
+    columnHeaderVertAlign:"bottom", //align header contents to bottom of cell
+	responsiveLayout: "hide", // hide rows that no longer fit
+
+    // Callbacks
+ 	rowSelected:function(row){
+ 		let pdburl = "models/" + row.getData().url;
+ 		let pdbname = row.getData().name;
+ 		loadMolecule(stage, pdburl)
+ 	},
+
+ 	rowDeselected:function(row){
+ 		let pdburl = "models/" + row.getData().url;
+ 		let pdbname = row.getData().name;
+ 		removeMolecule(stage, pdburl)
+ 	},
+
+});
