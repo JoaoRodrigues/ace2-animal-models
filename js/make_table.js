ae480cbdc38637c36174b838312b84cceeb0cbfa
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2020-05-02 14:49:07 -0700

    simplified JS and HTML

--- js/make_table.js
+++ js/make_table.js
@@ -1,5 +1,13 @@
+let getTooltip = function(column){
+    //column - column component
+
+    //function should return a string for the header tooltip of false to hide the tooltip
+    // return column.getDefinition().title;
+    return false
+}
+
 table = new Tabulator("#models-table", {
-	
+
 	// Data
 	ajaxURL: "models/models.json",
 	ajaxContentType: "json",
@@ -8,21 +16,21 @@ table = new Tabulator("#models-table", {
 	},
 	// Formatting
     columns: [
-        {title:"Model Name", field:"name", responsive: 0, widthGrow: 2},
-        {title:"Species", field:"species", responsive: 0, widthGrow: 2},
-        {title:"Score", field:"haddock_score", responsive: 0},
-        {title:"vdW", field:"e_vdw"},
-        {title:"Electrostatics", field:"e_elec"},
-        {title:"Desolvation", field:"e_desolv"},
-        {title:"BSA", field:"buried_surf_area"},
+        {title:"Model Name", field:"name", responsive: 0, widthGrow: 2, tooltip: false},
+        {title:"Species", field:"species", responsive: 0, widthGrow: 2, tooltip: false},
+        {title:"Score", field:"haddock_score", responsive: 0, headerTooltip: getTooltip},
+        {title:"vdW", field:"e_vdw", headerTooltip: getTooltip},
+        {title:"Electrostatics", field:"e_elec", headerTooltip: getTooltip},
+        {title:"Desolvation", field:"e_desolv", headerTooltip: getTooltip},
+        {title:"BSA", field:"buried_surf_area", headerTooltip: getTooltip},
     ],
     // Layout
     layout:"fitColumns",
     resizableColumns: false,
-	// groupBy:"species",
     selectable: true,
-    columnHeaderVertAlign:"bottom", //align header contents to bottom of cell
+    columnHeaderVertAlign: "bottom", //align header contents to bottom of cell
 	responsiveLayout: "hide", // hide rows that no longer fit
+
     pagination: "local",
     paginationSize: 5,  // model per page.
 
