3ec3f6fc7fe304e4c4f7b4061ac785dc60198786
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2020-05-02 17:01:32 -0700

    Improved layout

--- js/make_table.js
+++ js/make_table.js
@@ -1,50 +1,80 @@
 let getTooltip = function(column){
-    //column - column component
+  let d = column.getDefinition().description;
+  if (d) {
+    return d
+  }
 
-    //function should return a string for the header tooltip of false to hide the tooltip
-    // return column.getDefinition().title;
-    return false
+  return false
 }
 
 table = new Tabulator("#models-table", {
 
-	// Data
-	ajaxURL: "models/models.json",
-	ajaxContentType: "json",
-	ajaxResponse: function(url, params, response){
-		return response;
-	},
-	// Formatting
-    columns: [
-        {title:"Model Name", field:"name", responsive: 0, widthGrow: 2, tooltip: false},
-        {title:"Species", field:"species", responsive: 0, widthGrow: 2, tooltip: false},
-        {title:"Score", field:"haddock_score", responsive: 0, headerTooltip: getTooltip},
-        {title:"vdW", field:"e_vdw", headerTooltip: getTooltip},
-        {title:"Electrostatics", field:"e_elec", headerTooltip: getTooltip},
-        {title:"Desolvation", field:"e_desolv", headerTooltip: getTooltip},
-        {title:"BSA", field:"buried_surf_area", headerTooltip: getTooltip},
-    ],
-    // Layout
-    layout:"fitColumns",
-    resizableColumns: false,
-    selectable: true,
-    columnHeaderVertAlign: "bottom", //align header contents to bottom of cell
-	responsiveLayout: "hide", // hide rows that no longer fit
+  // Data
+  ajaxURL: "models/models.json",
+  ajaxContentType: "json",
+  ajaxResponse: function(url, params, response){
+    return response;
+  },
 
-    pagination: "local",
-    paginationSize: 5,  // model per page.
+  // Formatting
+  columns: [
+    {title:"Model Name", field:"name", responsive: 0, widthGrow: 2, minWidth: 150},
+    {title:"Species", field:"species", responsive: 0, widthGrow: 2, minWidth: 150},
+    {
+      title:"Score",
+      field:"haddock_score",
+      description:"HADDOCK score. A prediction of how well the proteins interact. Lower scores mean stronger (better) interactions.",
+      responsive: 0,
+      minWidth: 100
+    },
+    {
+      title:"vdW",
+      field:"e_vdw",
+      minWidth: 75,
+      description:"van der Waals energy of the interface in kcal/mol. Lower is better.",
+    },
+    {
+      title:"Electrostatics",
+      field:"e_elec",
+      minWidth: 75,
+      description:"Electrostatic energy of the interface in kcal/mol. Lower is better.",
+    },
+    {
+      title:"Desolvation",
+      field:"e_desolv",
+      minWidth: 75,
+      description:"Empirical desolvation energy of the interface. Lower is better.",
+    },
+    {
+      title:"BSA",
+      field:"buried_surf_area",
+      minWidth: 75,
+      description:"Buried Surface Area in sq A.",
+    },
+  ],
+
+  // Layout
+  layout:"fitColumns",
+  resizableColumns: false,
+  selectable: true,
+  columnHeaderVertAlign: "bottom", //align header contents to bottom of cell
+  responsiveLayout: "hide",
+
+  tooltipsHeader: getTooltip,
+  // pagination: "local",
+  // paginationSize: 10,  // model per page.
 
     // Callbacks
- 	rowSelected:function(row){
- 		let pdburl = "models/" + row.getData().url;
- 		let pdbname = row.getData().name;
- 		loadMolecule(stage, pdburl)
- 	},
+  rowSelected:function(row){
+    let pdburl = "models/" + row.getData().url;
+    let pdbname = row.getData().name;
+    loadMolecule(stage, pdburl)
+  },
 
- 	rowDeselected:function(row){
- 		let pdburl = "models/" + row.getData().url;
- 		let pdbname = row.getData().name;
- 		removeMolecule(stage, pdburl)
- 	},
+  rowDeselected:function(row){
+    let pdburl = "models/" + row.getData().url;
+    let pdbname = row.getData().name;
+    removeMolecule(stage, pdburl)
+  },
 
 });
